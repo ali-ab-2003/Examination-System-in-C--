daa9e8978d3eb78849314a0070184101274ed5e7
ali-ab-2003 <ali.abdulllah.003@gmail.com>
2023-05-05 23:44:56 +0500

    First commit

---
+++ examinationSystem.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include "examinationSystem.h"
+
+using namespace std;
+
+User::User()
+{
+    name = " ";
+    email = " ";
+    password = " ";
+}
+
+User::User(string Name, string Email, string Password)
+{
+    name = Name;
+    email = Email;
+    password = Password;
+}
+
+void User::setName(string n)
+{
+    name = n;
+}
+
+void User::setEmail(string Email)
+{
+    email = Email;
+}
+
+void User::setPassword(string p)
+{
+    password = p;
+}
+
+string User::getName()
+{
+    return name;
+}
+
+string User::getEmail()
+{
+    return email;
+}
+
+string User::getPassword()
+{
+    return password;
+}
